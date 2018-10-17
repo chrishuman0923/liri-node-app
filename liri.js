d3c154c4bbfdfd336dba05e43e50daff03b66495
chuman <42072991+chrishuman0923@users.noreply.github.com>
2018-10-17 19:58:28 -0400

    Adds require statements in liri.js to add functionality for spotify keys and other modules

--- liri.js
+++ liri.js
@@ -1,11 +1,24 @@
-require("dotenv").config();
+require('dotenv').config();
 
-var spotify = new Spotify(keys.spotify);
+//Modules needed to make the app work
+var keys = require('./keys'),
+    Spotify = require('node-spotify-api'),
+    moment = require('moment'),
+    request = require('request');
+
+//get spotify keys and passed in args
+var spotify = new Spotify(keys.spotify),
+    args = process.argv,
+    cmd = args[2],
+    val = args[3];
+
+console.log(cmd, val);
 
 /*
 COMMANDS FOR LIRI.JS
-concert-this
-spotify-this-song
-movie-this
-do-what-it-says
+node liri.js concert-this <artist/band name here>
+This will search the Bands in Town Artist Events API ("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp") for an artist and render the following information about each event to the terminal:
+Name of the venue
+Venue location
+Date of the Event (use moment to format this as "MM/DD/YYYY")
 */
